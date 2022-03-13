98953a1d29b082dc828aa003d1c1abb4b0e2ee65
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2022-03-13 02:24:08 -0600

    Impemented app, emitter, config

--- emisor/app.js
+++ emisor/app.js
@@ -1,24 +1,29 @@
-var Emitter = require("./emitter"); // Se manda llamar el modulo del archivo emitter.js
+//var Emitter = require("./emitter"); // Se comenta la funcion hecha anteriorimente
 
-var emtr = new Emitter(); //Ahora se crea un evento con el modulo anterior
+var Emitter = require("events"); //Se utiliza el Emitter de NODE JS
 
-emtr.on("greet", () => {
-  //ejecutamos el emtr.on para ingresar un array de greet con una string
+var config = require("./config"); //Se inyecta la dependencia del archivo config
+
+//Cuando se ejecuta casi no hay cambios a la hora de imprimir el codigo mas que con una linea que es la que imprime el console.log(emtr) ya que lo toma como evento y aparecen multiples lineas que no se habian visto antes como el un elemento oculto (_events) que da como resultado [Object: null prototype], tambien hay un _eventsCount con 2, un _maxListeners con indefinido y un [Symbol(kCapture)]: false
+
+var emtr = new Emitter();
+
+emtr.on(config.events.GREET, () => {
   console.log("Somewhere, someone said hello.");
-});
+}); //Se cambia la llamada al metodo con el objetivo de utilizar el modulo contruido en el archivo config para poner en practica las buenas costumbres de los desarrolladores para evitar errores con la conolcacion de los tipos de valores correctamente
 
-emtr.on("greet", () => {
-  //Ejecutamon el emtr.on para ingresar otro sting en greet
+emtr.on(config.events.GREET, () => {
   console.log("A greeting occurred!");
 });
 
 console.log("Hello!");
-emtr.emit("greet"); //ejecutamos todo lo que tenga greet en el Emitter
+emtr.emit(config.events.GREET);
 
-emtr.on("jump", () => {
-  //ahora ingresamos jump y un texto
+emtr.on(config.events.JUMP, () => {
   console.log("someone jumped!");
 });
 
-console.log(emtr); //ejecutamos todo el emtr el cual tiene la forma de un objeto
-emtr.emit("jump"); //ejecutamos el jump del Emitter
+console.log(emtr);
+emtr.emit(config.events.JUMP);
+
+// Se ha cambiado todas las llamadas al modulo y los cambios fueron nulos ya que estan recibiendo el mismo string solamente de la primera manera era colocado de manera directa pero ahora lo solicita al archivo config y lo coloca en el lugar que corresponde.
